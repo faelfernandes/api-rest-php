cd8257edbe769d49c9bd639243d9c4acee9b0d88
habbioficial <contato@habbi.com.br>
2021-11-09 09:16:24 -0300

    Testando funcionalidades

--- App/Controllers/GeralController.php
+++ App/Controllers/GeralController.php
@@ -8,7 +8,7 @@ class GeralController
   public function get($id = null) {
     if ($id) {
       if (!is_int(intval($id))) {
-        throw new \Exception("Parâmetro inválido");
+        return "teste";
       }
       return Geral::listarPorId($id);
     } else {
